32ed4f2560e451a93e8861cc5e701f655a47bf99
Tommy Long Pham <tpham@smugmug.com>
2020-02-18 14:25:09 -0800

    shoe example

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,24 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets(_, { input }, context, ) {
+      return context.models.Pet.findMany();
+    },
+    shoes(_, { input }) {
+      return [{brand: 'nike', size: 12}, {brand: 'adidas', size: 14}].filter(shoe => input.brand === shoe.brand);
     }
   },
-  User: {
+  // Mutation: {
     
-  }
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === 'DOG'
+  //       ? 'https://placedog.net/300/300'
+  //       : 'http://placekitten.com/300/300'
+  //   }
+  // },
+  // User: {
+    
+  // }
 }
--- api/src/schema.js
+++ api/src/schema.js
@@ -4,8 +4,37 @@ const { gql } = require('apollo-server')
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
 
+  type Shoe {
+    brand: String!
+    size: Int!
+  }
+
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String
+  }
+
+  input PetInput {
+    name: String
+    type: String
+  }
+
+  input ShoeInput {
+    brand: String
+    size: Int
+  }
+
+  type Query {
+    pets(input: PetInput): [Pet]!
+    shoes(input: ShoeInput): [Shoe]!
+  }
 `;
 
 module.exports = typeDefs
